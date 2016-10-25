eb94cf83523d2375aeeb56e2bdfe3880af0066a0
Raul Bocanegra Algarra <raul.bocanegra.algarra@ericsson.com>
2016-10-25 14:29:25 +0200

    update build.py
    Some mimnor refactor

--- build.py
+++ build.py
@@ -12,18 +12,7 @@ def extractReference():
          return reference
          break
 
-if __name__ == "__main__":
-   parser = argparse.ArgumentParser(description="This script build the corresponding package.",
-                                    prog="build.py",
-                                    formatter_class=argparse.RawTextHelpFormatter)
-   parser.add_argument("-l,", "--local", action='store_true', help='run local build')
-   parser.add_argument("-u", "--user", default="rndev", help='choose the user, \"cdev\" by default')
-   parser.add_argument("-c", "--channel", default="testing", help='choose the channel, \"testing\" by default')
-   parser.add_argument("-x86", "--build_x86", action='store_true', help='build x86 binaries')
-   parser.add_argument("-t", "--build_test", action='store_true', help='build tests')
-
-   args = parser.parse_args(args=sys.argv[1:])
-      
+def build(args):
    reference = extractReference()
    package_name = reference.split("/")[0] 
    version = reference.split("/")[1]
@@ -35,9 +24,24 @@ if __name__ == "__main__":
       os.environ["CONAN_USERNAME"] = args.user
       os.environ["CONAN_CHANNEL"] = args.channel
 
-   builder = ConanMultiPackager(reference=reference)    
+   build_args = " "
+   builder = ConanMultiPackager(reference=reference, args=build_args)    
    builder.add({"arch": "x86", "build_type": "Release", "compiler": "Visual Studio", "compiler.runtime":"MD" } )
    builder.add({"arch": "x86", "build_type": "Debug"  , "compiler": "Visual Studio", "compiler.runtime":"MDd"} )
    builder.add({"arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.runtime":"MD" } )
    builder.add({"arch": "x86_64", "build_type": "Debug"  , "compiler": "Visual Studio", "compiler.runtime":"MDd"} )
    builder.run()
+
+if __name__ == "__main__":
+   parser = argparse.ArgumentParser(description="This script build the corresponding package.",
+                                    prog="build.py",
+                                    formatter_class=argparse.RawTextHelpFormatter)
+   parser.add_argument("-l,", "--local", action='store_true', help='run local build')
+   parser.add_argument("-u", "--user", default="rndev", help='choose the user, \"cdev\" by default')
+   parser.add_argument("-c", "--channel", default="testing", help='choose the channel, \"testing\" by default')
+   #parser.add_argument("-x86", "--build_x86", action='store_true', help='build x86 binaries')
+   #parser.add_argument("-t", "--build_test", action='store_true', help='build tests')
+
+   args = parser.parse_args(args=sys.argv[1:])
+
+   build(args);
