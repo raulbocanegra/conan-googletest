365b60a226ff024bd6b0d41dd86e5d9daf2bfd12
Raul Bocanegra Algarra <raul.bocanegra.algarra@ericsson.com>
2016-10-25 12:12:38 +0200

    Update build.py
    Support for command line arguments

--- build.py
+++ build.py
@@ -1,35 +1,43 @@
 from conan.packager import ConanMultiPackager
-import os, re, subprocess
-
-############### CONFIGURE THESE VALUES ##################
-default_user = "demo"
-default_channel = "testing"
-#########################################################
+import os, sys, re, subprocess, argparse
 
 def extractReference():
    ref_pattern= r"(.+)@{1}PROJECT"
    ref_regex = re.compile(ref_pattern)
-   conan_info_output = subprocess.check_output("conan info")   
-   for line in conan_info_output.split("\n"):      
+   conan_info_output = subprocess.check_output("conan info")
+   for line in conan_info_output.split("\n"):
       result_match = re.match(ref_regex, line)
       if result_match:
-         reference = result_match.group(1)         
+         reference = result_match.group(1)
          return reference
          break
 
 if __name__ == "__main__":
+   parser = argparse.ArgumentParser(description="This script build the corresponding package.",
+                                    prog="build.py",
+                                    formatter_class=argparse.RawTextHelpFormatter)
+   parser.add_argument("-l,", "--local", action='store_true', help='run local build')
+   parser.add_argument("-u", "--user", default="rndev", help='choose the user, \"cdev\" by default')
+   parser.add_argument("-c", "--channel", default="testing", help='choose the channel, \"testing\" by default')
+   parser.add_argument("-x86", "--build_x86", action='store_true', help='build x86 binaries')
+   parser.add_argument("-t", "--build_test", action='store_true', help='build tests')
 
-    # This is only for local package testing, not Jenkins
-    username = os.getenv("CONAN_USERNAME", default_user)
-    channel = os.getenv("CONAN_CHANNEL", default_channel)
-
-    reference = extractReference()
-    package_name = reference.split("/")[0] 
-    version = reference.split("/")[1]
-    package = os.getenv("CONAN_PACKAGE", package_name)
-    os.environ["GOOGLETEST_VERSION"] = version   
+   args = parser.parse_args(args=sys.argv[1:])
+      
+   reference = extractReference()
+   package_name = reference.split("/")[0] 
+   version = reference.split("/")[1]
+   package = os.getenv("CONAN_PACKAGE", package_name)
+   os.environ["GOOGLETEST_VERSION"] = version   
     
-    builder = ConanMultiPackager(reference=reference, username=username, channel=channel)    
-    builder.add({"arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.runtime":"MD" } )
-    builder.add({"arch": "x86_64", "build_type": "Debug"  , "compiler": "Visual Studio", "compiler.runtime":"MDd"} )
-    builder.run()
+   if args.local:
+      # This is only for local package testing, not Jenkins this environment vars are allready set on Jenkins
+      os.environ["CONAN_USERNAME"] = args.user
+      os.environ["CONAN_CHANNEL"] = args.channel
+
+   builder = ConanMultiPackager(reference=reference)    
+   builder.add({"arch": "x86", "build_type": "Release", "compiler": "Visual Studio", "compiler.runtime":"MD" } )
+   builder.add({"arch": "x86", "build_type": "Debug"  , "compiler": "Visual Studio", "compiler.runtime":"MDd"} )
+   builder.add({"arch": "x86_64", "build_type": "Release", "compiler": "Visual Studio", "compiler.runtime":"MD" } )
+   builder.add({"arch": "x86_64", "build_type": "Debug"  , "compiler": "Visual Studio", "compiler.runtime":"MDd"} )
+   builder.run()
